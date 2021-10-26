05c3b366391b0798535aa0d47e1901d3bcea1c77
tiago_michel <tiago.silva@compasso.com.br>
2021-10-26 11:58:24 -0300

    Swagger Configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,11 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
 	implementation("io.springfox:springfox-swagger-ui:3.0.0")
 	implementation("io.springfox:springfox-swagger2:3.0.0")
+	implementation("io.springfox:springfox-boot-starter:3.0.0")
+
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.apache.kafka:kafka-streams")
 
--- src/main/kotlin/com/example/conf/SwaggerConfiguration.kt
+++ src/main/kotlin/com/example/conf/SwaggerConfiguration.kt
@@ -1,39 +1,45 @@
 package com.example.conf
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.service.ApiInfo
-import springfox.documentation.service.Contact
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
-import springfox.documentation.swagger2.annotations.EnableSwagger2
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux
+import java.time.YearMonth
 
 @Configuration
-@EnableSwagger2
-class SwaggerConfiguration {
+@EnableSwagger2WebFlux
+@ConditionalOnClass(Docket::class)
+@ConditionalOnMissingBean(Docket::class)
+class SwaggerConfiguration  {
+
+    @Value("\${SERVER_CONTEXT_PATH:/}")
+    private val contextPath: String? = null
 
     @Bean
     fun api(): Docket {
         return Docket(DocumentationType.SWAGGER_2)
+            .pathMapping(contextPath)
             .apiInfo(getApiInfo())
+            .directModelSubstitute(YearMonth::class.java, String::class.java)
             .select()
-            .apis(RequestHandlerSelectors.basePackage("com.example.controller"))
+            .apis(RequestHandlerSelectors.basePackage("com.example"))
             .paths(PathSelectors.any())
             .build()
     }
 
     private fun getApiInfo(): ApiInfo {
-        val contact = Contact("Tiago Michel", "http://wordpress.com", "@hotmail.com")
         return ApiInfoBuilder()
-            .title("Example Api Title")
-            .description("Example Api Definition")
+            .title("Documentação da minha API")
+            .description("spring-reactive-kotlin-gradle")
             .version("1.0.0")
-            .license("Apache 2.0")
-            .licenseUrl("https://start.spring.io/")
-            .contact(contact)
             .build()
     }
 }
